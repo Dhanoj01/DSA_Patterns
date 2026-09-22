03eb2ca668f6bac5fd77d3f2ba32b0d7074c3923
dhanojkumarsingh <dhanojkumar783@gmail.com>
2026-09-22 16:44:03 +0530

    DSA pattern series

---
+++ DSA_Patterns/src/Pattern_2Pointer/test.java
@@ -0,0 +1,12 @@
+package Pattern_2Pointer;
+
+public class test {
+
+	
+	public static void main(String args[])
+	{
+		
+		System.out.println("helloworld");
+		
+	}
+}
---
+++ DSA_Patterns/src/practise/prac.java
@@ -0,0 +1,24 @@
+package practise;
+
+public class prac {
+	
+	public static void main(String args[])
+	{
+		
+	
+
+		        int[] arr = {10, 20, 30, 20, 40, 10, 20, 20};
+
+		        for (int i = 0; i < arr.length; i++) {
+
+		            for (int j = i + 1; j < arr.length; j++) {
+
+		                if (arr[i] == arr[j]) {
+		                    System.out.println("Duplicate: " + arr[i]);
+		                    break;
+		                }
+		            }
+		        }
+	}
+
+}
